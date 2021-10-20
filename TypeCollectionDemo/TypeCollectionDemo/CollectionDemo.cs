e3080b9074811b70dcf2d76bc89def083d679435
darshanDhone <55595144+darshanDhone@users.noreply.github.com>
2021-10-20 19:13:02 +0530

    Add files via upload

---
+++ TypeCollectionDemo/TypeCollectionDemo/CollectionDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace TypeCollectionDemo
+{
+    class CollectionDemo
+    {
+        static void Main(string[] args)
+        {
+            #region
+            int[] numberArray = new int[3];
+            numberArray[0] = 454;
+            numberArray[1] = 4452;
+            numberArray[2] = 21;
+
+            for(int i = 0; i < numberArray.Length; i++)
+            {
+                Console.WriteLine(numberArray[i]);
+            }
+            #endregion
+
+            #region System.Collections classes
+
+            ArrayList countryList = new ArrayList();
+            countryList.Add("India");
+            countryList.Add("USA");
+            countryList.Add("China");
+            countryList.Insert(1, "UK");
+
+            //object initializer
+            Person person = new Person() { FirstName = "Darshan", LastName = "Dhone" };
+            Console.WriteLine(person.FirstName + " " + person.LastName);
+            countryList.Add(person);
+
+            Console.WriteLine(((Person)countryList[4]).FirstName);
+
+            for (int i = 0; i < countryList.Count; i++)
+            {
+                Console.WriteLine(countryList[i]);
+            }
+            #endregion
+
+
+        }
+    }
+}
---
+++ TypeCollectionDemo/TypeCollectionDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System; 
+
+namespace TypeCollectionDemo
+{
+    struct Employee
+    {
+        public int EmpNo;
+        public string EmpName;
+        public double Salary;
+
+        public double AnnualSalary()
+        {
+            return Salary * 12;
+        }
+
+        public Employee(int empno,string empname,double salary)
+        {
+            this.EmpNo = empno;
+            this.EmpName = empname;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"EmpNo: {EmpNo} EmpName: {EmpName} Salary: {Salary}");
+        }
+        
+    }
+    enum Month
+    {
+        jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(101, "darshan", 1000);
+            Console.WriteLine("" + employee.AnnualSalary());
+            Console.WriteLine(employee);
+
+            Console.WriteLine(DayOfWeek.Wednesday + ":" + (int)DayOfWeek.Wednesday);
+            Console.WriteLine(Month.oct + ":" + (int) Month.oct);
+        }
+    }
+}
