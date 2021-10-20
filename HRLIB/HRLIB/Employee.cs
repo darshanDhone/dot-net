9d2f3cdc814427ce20d9cfb296653cb9719d6fac
darshanDhone <55595144+darshanDhone@users.noreply.github.com>
2021-10-20 19:45:36 +0530

    Add files via upload

---
+++ HRLIB/HRLIB/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HRLIB
+{
+    public class Employee
+    {
+        public string name;
+
+        public string EName
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string address;
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public Employee()
+        {
+            this.EName = "Darshan";
+            this.Address = "Nagpur";
+        }
+        public Employee(string name, string add)
+        {
+            this.EName = name;
+            this.Address = add;
+        }
+    }
+}
