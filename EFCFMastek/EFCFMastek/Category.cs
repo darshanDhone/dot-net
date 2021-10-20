4434a975badc0b1df6dc24b004f5359aafd7f54c
darshanDhone <55595144+darshanDhone@users.noreply.github.com>
2021-10-20 19:05:35 +0530

    Add files via upload

---
+++ EFCFMastek/EFCFMastek/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCFMastek
+{
+    [Table("Category")]
+    class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName ="varchar(20)")]
+        public string CategoryName { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
